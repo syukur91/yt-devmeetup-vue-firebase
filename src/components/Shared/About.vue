3a7d6c29fbe8d12b0d70ca0b475b52ff1b1ba43b
syukur <syukur.jtk09@gmail.com>
2018-04-29 16:07:30 +0700

    edit about page

--- src/components/Shared/About.vue
+++ src/components/Shared/About.vue
@@ -2,16 +2,21 @@
   <v-container>
    
    <v-layout>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
+     <v-flex xs12 sm6 offset-sm3 >
+      <v-card class="grey darken-1 white--text">
         
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline mb-0">About Us</h3>
-            </br>
-            <div>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</div>
-          </div>
-        </v-card-title>
+ 
+
+          <v-card-title primary-title>
+            <div>
+          
+              <v-card-media src="/static/logo.png" height="125px" contain></v-card-media>
+              <h3 class="white--text headline">About Us</h3>
+              </br>
+              <div class="justified">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</div>
+            </div>
+
+          </v-card-title>
       </v-card>
     </v-flex>
   </v-layout>
